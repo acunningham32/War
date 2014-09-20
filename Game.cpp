f40206988a181fce83dab1a61514c4c12bb9e327
jasonwichert <wichertjj1@gcc.edu>
2014-09-20 12:47:50 -0400

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -28,19 +28,33 @@ void Game::playCard()
 			compAddCard(userCard, compCard);
 		}
 	}
+	return;
 }
 
 void Game::war(Card userCard, Card compCard)
 {
-	//Code to check if the players have less than 3 cards, if both have less, lowest numebr loses.
-	warCards.push_back(userCard);
-	warCards.push_back(compCard);
-	for(int i = 0; i < 3; i++)
+	if(userHand.size() < 3)
 	{
-		warCards.push_back(userHand.at(0));
-		userHand.remove(0);
-		warCards.push_back(compHand.at(0));
-		compHand.remove(0);
+		cout << "User is out of cards: Computer wins!" << endl;	
+		return;
 	}
-	playCard();
+	else if(compHand.size() < 3)
+	{
+		cout << "Computer is out of cards: User wins!" << endl;
+		return;
+	}
+	else
+	{
+		warCards.push_back(userCard);
+		warCards.push_back(compCard);
+		for(int i = 0; i < 3; i++)
+		{
+			warCards.push_back(userHand.at(0));
+			userHand.remove(0);
+			warCards.push_back(compHand.at(0));
+			compHand.remove(0);
+		}
+		playCard();
+	}
+	return;
 }
