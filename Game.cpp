f947b4aecd8585954bc30b8ed9b1894117e2bbcc
jasonwichert <wichertjj1@gcc.edu>
2014-09-20 11:59:25 -0400

    Create Game.cpp

---
+++ Game.cpp
@@ -0,0 +1,22 @@
+void Game::playCard()
+{
+	c_user = userHand.remove(0);
+	c_comp = compHand.remove(0);
+
+	if(c_user.getValue() > c_comp.getValue())
+	{
+		compAddCard(c_user, c_comp);
+	}
+	else if(tie(c_user, c_comp)
+	{
+		war(c_user, c_comp);
+	}
+	else
+	{
+		compAddCard(c_user, c_comp);
+	}	
+}
+
+void Game::war(Card c_user, Card c_comp)
+{
+	warCards.add
