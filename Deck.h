d3c8ddf80e041db767d8aeafe7a75e3d2d2f7e96
lewisjl1 <lewisjl1@gcc.edu>
2014-09-24 16:45:39 -0400

    Update Deck.h

--- Deck.h
+++ Deck.h
@@ -1,13 +1,26 @@
+/*
+  File: Deck.h
+  Author:  Jessica Lewis and Andrew Cunningham
+  Course:  COMP 220, Computer Programming II
+  Date:    22 September 2014
+  Description:  This file provides the definition of the data types
+				for the Deck class.
+*/
+
+#ifndef ___DECK_H
+#define ___DECK_H
+
 #include <vector>
-#include "Card.h"
 using std::vector;
 
 class Deck {
 public:
+	friend class Game;	//make game a friend class to deck allowing game to see functions and variables in deck
 	Deck();
 	void makeDeck();
 	void dealDeck();
 	vector <Card> deck;
-	vector <Card> userHand;
-	vector <Card> compHand;
+	vector <Card> userDeck;
+	vector <Card> compDeck;
 };
+#endif
