81719ad44015a6c06f23493f579a18e81d8fc631
lewisjl1 <lewisjl1@gcc.edu>
2014-09-24 16:47:08 -0400

    Update Game.h

--- Game.h
+++ Game.h
@@ -1,3 +1,13 @@
+/*
+  File: Game.h
+  Author:  Jessica Lewis
+  Course:  COMP 220, Computer Programming II
+  Date:    22 September 2014
+  Description:  This file provides the definitions of the data types
+				for the Game class. 
+*/
+
+#ifndef ___GAME_H
 #define ___GAME_H
 
 #include <vector>
@@ -5,16 +15,21 @@ using std::vector;
 
 class Game {
 public:
-	void playCard(Deck &myDeck);
+	Game();
+	Game(Deck d);
+	void playCard();
 	bool userWin(Card a, Card b);
 	bool tie(Card a, Card b);
-	void userAddCard(Deck &myDeck, Card a, Card b);
-	void compAddCard(Deck &myDeck, Card a, Card b);
-	void war(Deck &myDeck, Card a, Card b);
-	Game(Deck d);
+	void userAddCard(Card a, Card b);
+	void compAddCard(Card a, Card b);
+	void war(Card a, Card b);
+	bool isGameOver() {return gameOver;}
 private:
 	vector<Card> warCards;
-	Deck a;
+	vector<Card> userHand;
+	vector<Card> compHand;
+	Deck myDeck;
+	bool gameOver;
 };
 
 #endif
