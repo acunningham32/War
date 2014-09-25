997dcab469cf4994bebbdd6819849d9aa9be9bcc
jasonwichert <wichertjj1@gcc.edu>
2014-09-25 11:52:43 -0400

    Update Game.h

--- Game.h
+++ Game.h
@@ -20,12 +20,23 @@ public:
 	Game();
 	Game(Deck d);
 	void playCard();
-	bool userWin(Card a, Card b);
+	//Precondition: Called by a game object
+	//Postcondition: plays out a round of war, also handles wars (ties)
 	bool tie(Card a, Card b);
+	//Precondition: Card a and Card b must be passed into it as card objects
+	//Postcondition: returns true if it is a tie, false if it is not a tie
 	void userAddCard(Card a, Card b);
+	//Precondition: Card a and Card b must be card objects
+	//Postcondition: adds a, b, and the warCards vector to the user's hand
 	void compAddCard(Card a, Card b);
+	//Precondition: Card a and Card b must be card objects
+	//Postcondition: adds a, b, and the warCards vector to the computer's hand
 	void war(Card a, Card b);
+	//Precondition: called by playCard if tie() returns true, a and b must be card objects
+	//Postcondition: plays out a war (3 cards from each player, playCard() for the 4th card
 	bool isGameOver() {return gameOver;}
+	//Preconditon: called outside of Game
+	//Postcondition: returns the value of gameOver
 private:
 	vector<Card> warCards;
 	vector<Card> userHand;
